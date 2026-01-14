3ebbb3b082f0d620c55cac869bfa3f310b0216f1
aidenbodily <aidenbod@icloud.com>
2026-01-14 15:27:50 -0700

    added computational thinking comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // I added a comment on the next one for my computational thinking so I thought I would go back and explain, I just freeballed this one it was easy enough
         Console.Write("What is your First name? ");
         string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
