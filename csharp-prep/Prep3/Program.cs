827e1b18d36a7546693f183c2ef56cfbd396de5a
aidenbodily <aidenbod@icloud.com>
2026-01-21 22:25:27 -0700

    Finished Prep assignment 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // completely forgot to do computational thinking on this one i just thugged it out
+        string playagain = "yes";
+        while(playagain == "yes")
+            {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            // Console.Write("What is the magic number? ");
+            // string magic = Console.ReadLine();
+            // int magicNumber = int.Parse(magic);
+            int guessNumber;
+            int guesses = 0;
+            do
+            {
+                guesses++;
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                if(guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if(guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                
+            }
+            while (guessNumber != magicNumber);
+            Console.WriteLine($"You guessed it! It took you {guesses} guesses!");
+            Console.WriteLine("Do you want to play again? (yes/no) ");
+            playagain = Console.ReadLine();
+        }
     }
 }
