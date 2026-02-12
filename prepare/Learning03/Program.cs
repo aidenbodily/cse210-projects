f3a8ad4cd09a17060e236b4dc98a580ce7c2c878
aidenbodily <aidenbod@icloud.com>
2026-02-12 12:50:29 -0700

    fraction program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
+        Console.WriteLine("\nRandom Fractions:");
+        Fraction randomFraction = new Fraction();
+        Random random = new Random();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            int top = random.Next(1, 11);
+            int bottom = random.Next(1, 11);
+            randomFraction.SetTop(top);
+            randomFraction.SetBottom(bottom);
+            Console.WriteLine($"Fraction {i}: string: {randomFraction.GetFractionString()} Number: {randomFraction.GetDecimalValue()}");
+        }
     }
 }
