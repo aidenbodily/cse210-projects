e70243aec70ed28a39c40a77f5c030873c65c878
aidenbodily <aidenbod@icloud.com>
2026-03-11 21:31:42 -0600

    finished the shape polymorhpialismsms assignment thingy

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // polymorphism: same loop, different GetArea() for each shape type
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("blue", 4, 6));
+        shapes.Add(new Circle("green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: " + shape.GetColor() + ", Area: " + shape.GetArea());
+        }
     }
 }
